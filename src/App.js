6473ebca18aab7d15017fea9ffcbc74369c691cc
rfpoulos <rfpoulos@outlook.com>
2018-08-22 21:54:59 -0400

    create account works

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Switch,
 } from 'react-router-dom';
 import Menu from './screens/menu/menu';
+import CreateAccount from './screens/create-account/create-account';
 
 export default () => 
   <Router>
@@ -12,6 +13,7 @@ export default () =>
       <Menu />
       <div>
         <Switch>
+          <Route path="/createaccount" component={ CreateAccount } />
         </Switch>
       </div>
     </div>
---
+++ src/screens/create-account/create-account.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { createAccount } from './create-account-helpers';
+import { connect } from 'react-redux';
+import {
+    withRouter,
+    Link,
+} from 'react-router-dom';
+import { 
+    compose, 
+    withState, 
+    withHandlers,
+} from 'recompose';
+import { updateUserObject } from '../../redux/actions';
+import TextInput from '../../components/text-input/text-input';
+import Button from '../../components/button/button';
+import {
+    container,
+    input,
+} from './create-account-style';
+
+export let CreateAccount = ({ 
+    createAccountForm,
+    handleForm, 
+    updateUserObject,
+    history,
+}) =>
+    <div style={ container }>
+        <div style={ input }>
+            <TextInput type="email" 
+                placeholder="Email"
+                value={ createAccountForm.email }
+                onChange={ handleForm('email') }
+            />
+        </div>
+        <div style={ input }>        
+            <TextInput type="text" 
+                placeholder="Username"
+                value={ createAccountForm.username }
+                onChange={ handleForm('username') }
+            />
+            </div>
+        <div style={ input }>        
+            <TextInput type="password"
+                placeholder="Password"
+                value={ createAccountForm.password }
+                onChange={ handleForm('password') }
+            />
+        </div>
+        <Button text="Create Account" 
+            onClick={ 
+                createAccount(createAccountForm, updateUserObject, history)
+        } />
+        <Link to="/signin">Already a user?  Sign in here.</Link>
+    </div>
+
+export let enhance = compose(
+    withRouter,
+    withState(
+        'createAccountForm',
+        'updateCreateAccountForm',
+        { email: '', password: '', username: '' }
+    ),
+    withHandlers({
+        handleForm:({ 
+                createAccountForm, 
+                updateCreateAccountForm,
+            }) => (category) => event => 
+                updateCreateAccountForm({
+                    ...createAccountForm, 
+                    [category]: event.target.value
+            }),
+    }),
+);
+
+export default enhance(CreateAccount);
